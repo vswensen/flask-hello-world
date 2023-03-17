98ade0f0990c91ae81a2e2e5b0ab9ef0ebdf609e
vswensen <tory.swensen@gmail.com>
2023-03-17 00:42:12 +0000

    Create db_create

--- app.py
+++ app.py
@@ -14,3 +14,20 @@ def testing():
     conn = psycopg2.connect(DB_URL)
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def creating():
+    conn = psycopg2.connect(DB_URL)
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
